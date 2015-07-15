24c499fd92fa8230212a1d49c05cba8d5336c19c
Fercho1992 <fernandoalex_92@hotmail.com>
2015-07-14 19:45:29 -0500

    change index

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php 
+$conn = new Mysqli($host, $usuario,$cont,$bdd);
+
+if($conn->connect_error)
+	die ($conn->connect_error);
+
 echo '<p>Hola Mundo</p>'; 
-$url=parse_url(getenv("CLEAR"))
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server=$url["host"];
+$username=$url["user"];
+$=$url["host"];
+$server=$url["host"];
 ?>
 
